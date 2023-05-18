eb93a66b46821b50b107bd2b18dd02afc0859bf6
shyamdharanm <shyamdharan214@gmail.com>
2023-05-18 11:48:58 +0530

    v

--- script1.js
+++
@@ -1,9 +0,0 @@
-var request=new XMLHttpRequest();
-fetch('https://restcountries.com/v2/all')
-  .then(response => response.json())
-  .then(data => {
-    const europeCountries = data.filter(country => country.region === 'Europe');
-    const totalPopulation = europeCountries.reduce((acc, country) => acc + country.population, 0);
-    console.log(`Total population of Europe region: ${totalPopulation}`);
-  })
-  .catch(error => console.log(error));
